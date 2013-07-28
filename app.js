76000720510f3a60fea42db0ad0f85d43f4ac5a6
Zainul Shah <zan2434@gmail.com>
2013-07-28 02:04:36 -0700

    socket io works on the server

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , UUID = require('node-uuid')
   , path = require('path');
 
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -37,6 +40,47 @@ app.post('/view-hierarchy', function (req, res) {
   console.log(req.body);
   res.send("ok");
 });
+
+var sio = io.listen(server);
+
+server.listen(8000);
+
+sio.configure(function () { 
+  sio.set("transports", ["xhr-polling"]); 
+  sio.set("polling duration", 10); 
+});
+
+sio.configure(function (){
+    sio.set('log level', 3);
+    sio.set('authorization', function (handshakeData, callback) {
+      callback(null, true); // error first callback style
+    });
+});
+
+//socket connection
+sio.sockets.on('connection', function (client) {
+
+	client.emit('onConnected', { 'status': 'OK' } );
+	console.log(' connected. '); //Useful to know when someone connects
+
+    //They send messages here, and we send them to the server to handle.
+	client.on('message', function(m) {
+	    console.log(m);
+	}); //client.on message
+
+	client.on('updateScreen', function (data) {
+		client.broadcast.emit('screenUpdated', data);
+		console.log(data);
+	});
+
+	client.on('updateElements', function (data) {
+	    client.broadcast.emit('updateElements', data);
+	    console.log(data);
+	});
+
+	console.log("sio is now configured to poll every 10 ms");
+
+}); //sockets onconnection
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
